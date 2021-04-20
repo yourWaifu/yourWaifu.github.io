7e126a752897c945af2f88ba5221dcb848d3dd47
Sleepy Flower Girl <wuhao64@gmail.com>
2021-04-20 19:12:46 -0400

    remove fade out before unloading causes too many issues

--- lib/fade-out.ts
+++ lib/fade-out.ts
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { SpringValue } from "react-spring";
 
 let listOfValuesToFadeOut = new Map<number, {
-    value: SpringValue<number>, to: number
+    value: SpringValue<number>, from: number, to: number
 }>();
 
 export function fadeOut() {
@@ -11,17 +11,19 @@ export function fadeOut() {
         value => {value.value.start({to: value.to})});
 }
 
+export function fadeIn() {
+    Array.from(listOfValuesToFadeOut.values()).map(
+        value => {value.value.start({to: value.from})});
+}
+
 let fadeOutCounter = 0;
-export function useFadeOut(value: SpringValue<number>, to: number, router: NextRouter) {
+export function useFadeOut(value: SpringValue<number>, from: number, to: number, router: NextRouter) {
     useEffect(() => {
         const index = fadeOutCounter;
-        listOfValuesToFadeOut.set(index, {value, to});
+        listOfValuesToFadeOut.set(index, {value, from, to});
         fadeOutCounter += 1;
         return () => {
             listOfValuesToFadeOut.delete(index);
         }
     }, []);
-    useEffect(() => {
-        window.addEventListener("beforeunload", fadeOut);
-    })
 }
